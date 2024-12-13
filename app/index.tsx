41f3ee764520bd39b18c7e312af70577c631f925
Christopher Lai <clai20707@gmail.com>
2024-12-12 21:14:08 -0500

    Remove file

--- app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function Index() {
-  return (
-    <View className="container bg-slate-900 flex-1 justify-center items-center">
-      <Text className="text-white text-xl font-bold">Hello.</Text>
-    </View>
-  );
-}
