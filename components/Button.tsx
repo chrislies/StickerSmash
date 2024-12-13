515d66f64d2c5928a16c22df875adab39919c17c
Christopher Lai <clai20707@gmail.com>
2024-12-13 02:26:43 -0500

    Add logic handling for onPress function

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,15 +4,16 @@ import FontAwesome from "@expo/vector-icons/FontAwesome";
 type Props = {
   label: string;
   theme?: "primary";
+  onPress?: () => void;
 };
 
-export default function Button({ label, theme }: Props) {
+export default function Button({ label, theme, onPress }: Props) {
   if (theme === "primary") {
     return (
       <View className="buttonContainer border-4 border-[#ffd33d] rounded-[18px]">
         <Pressable
           className="button bg-white"
-          onPress={() => alert("Button pressed")}
+          onPress={onPress}
         >
           <FontAwesome
             name="picture-o"
