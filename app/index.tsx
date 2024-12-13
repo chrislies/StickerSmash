aa349a70975fff1b549595dbed88f51282b6349d
Christopher Lai <clai20707@gmail.com>
2024-12-12 19:52:52 -0500

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="container bg-slate-900 flex-1 justify-center items-center">
+      <Text className="text-white text-xl font-bold">Hello.</Text>
+    </View>
+  );
+}
