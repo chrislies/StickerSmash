1c75e70ab9ad9139c4f16bf95c7000303090fa67
Christopher Lai <clai20707@gmail.com>
2024-12-12 23:37:37 -0500

    Add button props

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,22 @@
 import { Text, View } from "react-native";
-import { Link } from "expo-router";
+import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
+
+const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
   return (
     <View className="container">
-      <Text className="text">Hdellod.</Text>
-      <Link
-        href="/about"
-        className="text underline hover:opacity-50"
-      >
-        Go to About screen
-      </Link>
+      <View className="flex-1 pt-[28px]">
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View className="mb-8 items-center">
+        <Button
+          theme="primary"
+          label="Choose a photo"
+        />
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
