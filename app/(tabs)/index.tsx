7fe4741821fdbccbd6fee1f2c4091362642810eb
Christopher Lai <clai20707@gmail.com>
2024-12-16 23:45:24 -0500

    Add ternary operator for showAppOptions

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,11 +3,14 @@ import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
 import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
+import CircleButton from "@/components/CircleButton";
+import IconButton from "@/components/IconButton";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+  const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -18,10 +21,20 @@ export default function Index() {
 
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
     } else {
       alert("No image selected");
     }
   };
+
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {};
+
+  const onSaveImageAsync = async () => {};
+
   return (
     <View className="container">
       <View className="flex-1 pt-[28px]">
@@ -30,14 +43,35 @@ export default function Index() {
           selectedImage={selectedImage}
         />
       </View>
-      <View className="mb-8 items-center">
-        <Button
-          theme="primary"
-          label="Choose a photo"
-          onPress={pickImageAsync}
-        />
-        <Button label="Use this photo" />
-      </View>
+      {showAppOptions ? (
+        <View className="absolute bottom-[80px]">
+          <View className="flex-row items-center">
+            <IconButton
+              icon="refresh"
+              label="Reset"
+              onPress={onReset}
+            />
+            <CircleButton onPress={onAddSticker}></CircleButton>
+            <IconButton
+              icon="save-alt"
+              label="Save"
+              onPress={onSaveImageAsync}
+            />
+          </View>
+        </View>
+      ) : (
+        <View className="mb-8 items-center">
+          <Button
+            theme="primary"
+            label="Choose a photo"
+            onPress={pickImageAsync}
+          />
+          <Button
+            label="Use this photo"
+            onPress={() => setShowAppOptions(true)}
+          />
+        </View>
+      )}
     </View>
   );
 }
